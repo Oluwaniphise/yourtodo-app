f112913f37bdd1bdb5fb8c542b69e008c5a82acd
Enoch Oduyale <oduyaleenoch@gmail.com>
2020-06-09 01:38:09 -0700

    another commit

--- Todo/urls.py
+++ Todo/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('TodoList.urls')),
+    path('/', include('TodoList.urls')),
     path('admin/', admin.site.urls),
     
   
